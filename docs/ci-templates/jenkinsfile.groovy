ea53161f739c068253cb932e895d729f9ddec05c
RobinT <robin.trassard@gmail.com>
2026-04-27 00:13:53 +0200

    chore: bump to version 0.5.8

--- docs/ci-templates/jenkinsfile.groovy
+++ docs/ci-templates/jenkinsfile.groovy
@@ -66,7 +66,7 @@ pipeline {
     }
 
     environment {
-        PERF_SENTINEL_VERSION = '0.5.7'
+        PERF_SENTINEL_VERSION = '0.5.8'
         PERF_SENTINEL_TRACES  = 'target/traces.json'
         PERF_SENTINEL_CONFIG  = '.perf-sentinel.toml'
     }
