03c68c1cba33ecd3b48da4c42c12e3cacfec7093
RobinT <robin.trassard@gmail.com>
2026-06-21 08:34:13 +0200

    chore(release): 0.8.14 (chart 0.2.63)

--- docs/ci-templates/jenkinsfile.groovy
+++ docs/ci-templates/jenkinsfile.groovy
@@ -66,7 +66,7 @@ pipeline {
     }
 
     environment {
-        PERF_SENTINEL_VERSION = '0.8.13'
+        PERF_SENTINEL_VERSION = '0.8.14'
         PERF_SENTINEL_TRACES  = 'target/traces.json'
         PERF_SENTINEL_CONFIG  = '.perf-sentinel.toml'
     }
